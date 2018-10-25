0d3edc8a18877bee202113118c456543ccb4548f
Brian Pepple <bdpepple@gmail.com>
2018-10-25 16:41:38 -0400

    Set up postgresql db

--- metron/settings.py
+++ metron/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'metron.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'metron',
+        'USER': 'bpepple',
+        'PASSWORD': 'bamfpassword',
+        'HOST': '',
+        'PORT': '',
     }
 }
 
