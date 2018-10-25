590ba22692ef391f5ed1a8fb776f5c8f7082f6f1
Brian Pepple <bdpepple@gmail.com>
2018-10-25 18:46:35 -0400

    Add the debug_toolbar to the project url

--- metron/urls.py
+++ metron/urls.py
@@ -13,9 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf import settings
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
+
+if settings.DEBUG:
+    import debug_toolbar
+    urlpatterns += [
+        path('__debug__/', include(debug_toolbar.urls)),
+    ]
