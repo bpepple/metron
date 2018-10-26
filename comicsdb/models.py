a3f9151d33545f5d8a276c3dd03f75696837462c
Brian Pepple <bdpepple@gmail.com>
2018-10-26 09:24:03 -0400

    Add initial Publisher & Series models.

--- comicsdb/models.py
+++ comicsdb/models.py
@@ -1,3 +1,52 @@
 from django.db import models
 
-# Create your models here.
+
+class Publisher(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    founded = models.DateField(blank=True)
+    modified = models.DateField(auto_now=True)
+    short_desc = models.CharField(max_length=350)
+    desc = models.TextField(blank=True)
+    image = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True)
+
+    @property
+    def series_count(self):
+        return self.series_set.all().count()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class SeriesType(models.Model):
+    name = models.CharField(max_length=255)
+    notes = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Series(models.Model):
+    name = models.CharField(max_length=255)
+    sort_name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    volume = models.PositiveSmallIntegerField(blank=True)
+    year_began = models.PositiveSmallIntegerField(blank=True)
+    year_end = models.PositiveSmallIntegerField(blank=True)
+    type = models.ForeignKey(SeriesType, on_delete=models.CASCADE)
+    publisher = models.ForeignKey(Publisher, on_delete=models.CASCADE)
+    short_desc = models.CharField(max_length=350)
+    desc = models.TextField(blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name_plural = 'Series'
+        ordering = ['sort_name', 'year_began']
