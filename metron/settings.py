790153a95eccaf63d3f0b87efd0d98a68ffdec7d
Brian Pepple <bdpepple@gmail.com>
2018-10-26 15:44:21 -0400

    Add static image-not-found.png to project

--- metron/settings.py
+++ metron/settings.py
@@ -123,6 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
