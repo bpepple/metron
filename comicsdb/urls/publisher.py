65b1dc48904b75c114b70523e0e97ca858fa5638
Brian Pepple <bdpepple@gmail.com>
2018-10-27 16:04:41 -0400

    Add basic search to the publisher_list template

--- comicsdb/urls/publisher.py
+++ comicsdb/urls/publisher.py
@@ -1,10 +1,13 @@
-from django.urls import path
+from django.urls import path, re_path
 
-from comicsdb.views import PublisherList, PublisherDetail
+from comicsdb.views import (PublisherList, PublisherDetail,
+                            SearchPublisherList)
 
 
 app_name = 'publisher'
 urlpatterns = [
     path('page<int:page>/', PublisherList.as_view(), name='list'),
-    path('<slug:slug>/', PublisherDetail.as_view(), name='detail')
+    path('<slug:slug>/', PublisherDetail.as_view(), name='detail'),
+    re_path(r'^search/(?:page(?P<page>\d+)/)?$',
+            SearchPublisherList.as_view(), name='search')
 ]
--- comicsdb/views.py
+++ comicsdb/views.py
@@ -1,3 +1,7 @@
+from functools import reduce
+import operator
+
+from django.db.models import Q
 from django.views.generic import ListView, DetailView
 
 from comicsdb.models import Publisher
@@ -13,3 +17,17 @@ class PublisherList(ListView):
 
 class PublisherDetail(DetailView):
     model = Publisher
+
+
+class SearchPublisherList(PublisherList):
+
+    def get_queryset(self):
+        result = super(SearchPublisherList, self).get_queryset()
+        query = self.request.GET.get('q')
+        if query:
+            query_list = query.split()
+            result = result.filter(
+                reduce(operator.and_,
+                       (Q(name__icontains=q) for q in query_list)))
+
+        return result
