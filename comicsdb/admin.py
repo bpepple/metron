9d7e848c7689d0a0d9302b55c963bdd0751d7eb9
Brian Pepple <bdpepple@gmail.com>
2018-10-29 10:42:56 -0400

    Add the Series & SeriesType models to the admin page

--- comicsdb/admin.py
+++ comicsdb/admin.py
@@ -1,6 +1,6 @@
 from django.contrib import admin
 
-from comicsdb.models import Publisher
+from comicsdb.models import Publisher, Series, SeriesType
 
 
 @admin.register(Publisher)
@@ -8,8 +8,20 @@ class PublisherAdmin(admin.ModelAdmin):
     prepopulated_fields = {'slug': ('name',)}
     list_display = ('name', 'series_count',)
     readonly_fields = ('modified',)
-    fieldsets = (
-        (None, {
-            'fields': ('name', 'slug', 'modified', 'founded', 'short_desc', 'desc', 'image')
-        }),
-    )
+    fields = ('name', 'slug', 'modified', 'founded', 'short_desc',
+              'desc', 'image')
+
+
+@admin.register(Series)
+class SeriesAdmin(admin.ModelAdmin):
+    search_fields = ('name',)
+    list_display = ('name', 'year_began')
+    list_filter = ('publisher',)
+    prepopulated_fields = {'slug': ('name',)}
+    fields = ('name', 'slug', 'sort_name', 'publisher', 'volume',
+              'year_began', 'year_end', 'type', 'short_desc', 'desc')
+
+
+@admin.register(SeriesType)
+class SeriesTypeAdmin(admin.ModelAdmin):
+    fields = ('name', 'notes')
