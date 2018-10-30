94c9731b1140ec2f75ab23b4a1af0931042b649e
Brian Pepple <bdpepple@gmail.com>
2018-10-29 22:59:39 -0400

    Add Issue, Credits, Creator, and Role models

--- comicsdb/models.py
+++ comicsdb/models.py
@@ -2,6 +2,20 @@ from django.db import models
 from django.urls import reverse
 
 
+class Creator(models.Model):
+    name = models.CharField(max_length=255)
+    slug = models.SlugField(max_length=255, unique=True)
+    desc = models.TextField('Description', blank=True)
+    birth = models.DateField(null=True, blank=True)
+    image = models.ImageField(upload_to='images/%Y/%m/%d/', blank=True)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
 class Publisher(models.Model):
     name = models.CharField(max_length=255)
     slug = models.SlugField(max_length=255, unique=True)
@@ -17,6 +31,17 @@ class Publisher(models.Model):
     @property
     def series_count(self):
         return self.series_set.all().count()
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        ordering = ['name']
+
+
+class Role(models.Model):
+    name = models.CharField(max_length=25)
+    notes = models.TextField(blank=True)
 
     def __str__(self):
         return self.name
@@ -54,6 +79,40 @@ class Series(models.Model):
     def __str__(self):
         return f'{self.name} ({self.year_began})'
 
+    @property
+    def issue_count(self):
+        return self.issue_set.all().count()
+
     class Meta:
         verbose_name_plural = 'Series'
         ordering = ['sort_name', 'year_began']
+
+
+class Issue(models.Model):
+    series = models.ForeignKey(Series, on_delete=models.CASCADE)
+    name = models.CharField(max_length=255, blank=True)
+    slug = models.SlugField(max_length=255, unique=True)
+    number = models.CharField(max_length=25)
+    cover_date = models.DateField('Cover Date', null=True, blank=True)
+    store_date = models.DateField('In Store Date', null=True, blank=True)
+    desc = models.TextField('Description', blank=True)
+    image = models.ImageField('Cover', upload_to='images/%Y/%m/%d/',
+                              blank=True)
+    creators = models.ManyToManyField(Creator, through='Credits', blank=True)
+
+    def __str__(self):
+        return f'{self.series.name} #{self.number}'
+
+    class Meta:
+        ordering = ['series__name', 'cover_date', 'number']
+
+
+class Credits(models.Model):
+    creator = models.ForeignKey(Creator, on_delete=models.CASCADE)
+    issue = models.ForeignKey(Issue, on_delete=models.CASCADE)
+    role = models.ManyToManyField(Role)
+
+    class Meta:
+        verbose_name_plural = "Credits"
+        unique_together = ['creator', 'issue']
+        ordering = ['creator__name']
