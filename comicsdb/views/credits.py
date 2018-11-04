78a2dcb090fe05aff77c21cc8f9132e3ebfbb384
Brian Pepple <bdpepple@gmail.com>
2018-11-04 11:29:59 -0500

    Fix the CreditsCreate view success_url

--- comicsdb/views/credits.py
+++ comicsdb/views/credits.py
@@ -1,5 +1,5 @@
 from django.shortcuts import get_object_or_404
-from django.urls import reverse_lazy
+from django.urls import reverse
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
 
 from comicsdb.forms.credits import CreditsForm
@@ -10,8 +10,11 @@ class CreditsCreate(CreateView):
     model = Credits
     form_class = CreditsForm
     template_name = 'comicsdb/credits_form.html'
-    # TODO: Need to reverse to the referring issue detail
-    success_url = reverse_lazy('issue:list', kwargs={'page': 1})
+
+    def get_success_url(self):
+        slug = self.kwargs.get('slug', self.request.POST.get('slug'))
+
+        return reverse('issue:detail', kwargs={'slug': slug})
 
     def get_initial(self):
         """Calculate Initial Data for the form, validate ownership of issue """
