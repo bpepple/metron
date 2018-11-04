559c4d4f78632075ff385600ca87e153c58d18d2
Brian Pepple <bdpepple@gmail.com>
2018-11-04 10:02:05 -0500

    Add storyarc field to Issue templated & form

--- comicsdb/forms/issue.py
+++ comicsdb/forms/issue.py
@@ -1,5 +1,5 @@
 from django.forms import (ModelForm, TextInput, Textarea, Select,
-                          SelectDateWidget, ClearableFileInput)
+                          SelectMultiple, SelectDateWidget, ClearableFileInput)
 import datetime
 from comicsdb.models import Issue
 
@@ -10,12 +10,13 @@ class IssueForm(ModelForm):
 
     class Meta:
         model = Issue
-        exclude=('creators',)
+        exclude = ('creators',)
         widgets = {
             'series': Select(),
             'name': TextInput(attrs={'class': 'input'}),
             'slug': TextInput(attrs={'class': 'input'}),
             'number': TextInput(attrs={'class': 'input'}),
+            'arcs': SelectMultiple(),
             'cover_date': SelectDateWidget(empty_label=("Choose Year", "Choose Month", "Choose Day"),
                                            years=YEARS),
             'store_date': SelectDateWidget(empty_label=("Choose Year", "Choose Month", "Choose Day"),
