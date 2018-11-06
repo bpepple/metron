f65e4100058ff7639fa1a6378f24fe06aead73a8
Brian Pepple <bdpepple@gmail.com>
2018-11-06 11:06:44 -0500

    Add django-widget-tweaks to INSTALLED_APPS

--- metron/settings.py
+++ metron/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'widget_tweaks',
     'sorl.thumbnail',
     'comicsdb'
 ]
