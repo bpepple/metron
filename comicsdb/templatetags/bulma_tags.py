be03f51fa9ee6d8acb08cf65c9d2c1145a677fa7
Brian Pepple <bdpepple@gmail.com>
2018-11-06 17:24:38 -0500

    Add some template tags to find select & multiselect fields

---
+++ comicsdb/templatetags/bulma_tags.py
@@ -0,0 +1,17 @@
+from django import forms
+from django import template
+
+register = template.Library()
+
+@register.filter
+def widget_type(field):
+    return field.field.widget
+
+@register.filter
+def is_select(field):
+    return isinstance(field.field.widget, forms.Select)
+
+
+@register.filter
+def is_multiple_select(field):
+    return isinstance(field.field.widget, forms.SelectMultiple)
