87a5b9cd767e56d1b01647a7ff49e7681736a995
Brian Pepple <bdpepple@gmail.com>
2018-11-08 22:55:59 -0500

    Add the Flatpage Middleware for 404 errors

--- metron/settings.py
+++ metron/settings.py
@@ -53,6 +53,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'django.contrib.flatpages.middleware.FlatpageFallbackMiddleware',
 ]
 
 ROOT_URLCONF = 'metron.urls'
