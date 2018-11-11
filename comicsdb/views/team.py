7fff33f6d678dea05bb4337ddc54395b0bded760
Brian Pepple <bdpepple@gmail.com>
2018-11-11 10:00:12 -0500

    Use the Team total count instead of the pagination count

--- comicsdb/views/team.py
+++ comicsdb/views/team.py
@@ -18,6 +18,11 @@ class TeamList(ListView):
     model = Team
     paginate_by = PAGINATE
 
+    def get_context_data(self, **kwargs):
+        context = super(TeamList, self).get_context_data(**kwargs)
+        context['count'] = self.get_queryset().count()
+        return context
+
 
 class TeamDetail(DetailView):
     model = Team
