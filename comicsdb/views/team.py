8bc8e6d87e83f48dc083ce9fdcc00434f67cb129
Brian Pepple <bdpepple@gmail.com>
2018-11-17 23:09:04 -0500

    Use the paginator obj to get the total team record counts

--- comicsdb/views/team.py
+++ comicsdb/views/team.py
@@ -18,11 +18,6 @@ class TeamList(ListView):
     model = Team
     paginate_by = PAGINATE
 
-    def get_context_data(self, **kwargs):
-        context = super(TeamList, self).get_context_data(**kwargs)
-        context['count'] = self.get_queryset().count()
-        return context
-
 
 class TeamDetail(DetailView):
     model = Team
