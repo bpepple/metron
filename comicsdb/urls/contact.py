f8d32a8f9147c302134d95377ed458bfc57b1c3d
Brian Pepple <bdpepple@gmail.com>
2018-11-27 10:34:27 -0500

    Add view allowing users to contact us.

---
+++ comicsdb/urls/contact.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+from comicsdb.views.contact import EmailView, SuccessView
+
+
+app_name = 'contact'
+urlpatterns = [
+    path('email/', EmailView, name='email'),
+    path('success/', SuccessView, name='success'),
+]
---
+++ comicsdb/views/contact.py
@@ -0,0 +1,30 @@
+from django.core.mail import send_mail, BadHeaderError
+from django.http import HttpResponse
+from django.shortcuts import render, redirect
+from django.template.loader import get_template
+
+from comicsdb.forms.contact import ContactForm
+
+
+def EmailView(request):
+    if request.method == 'GET':
+        form = ContactForm()
+    else:
+        form = ContactForm(request.POST)
+        if form.is_valid():
+            email = form.cleaned_data['email']
+            subject = form.cleaned_data['subject']
+            message = form.cleaned_data['message']
+            try:
+                send_mail(subject, message, email, ['bdpepple@gmail.com'])
+            except BadHeaderError:
+                return HttpResponse('Invalid header found.')
+            return redirect('contact:success')
+    return render(request, "comicsdb/contact-us.html", {'form': form})
+
+
+def SuccessView(request):
+    t = get_template('comicsdb/contact-success.html')
+    message = 'Success! Thank you for contacting us.'
+    html = t.render({'msg': message})
+    return HttpResponse(html)
