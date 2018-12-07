df1f48ab2bcd5fbc2505eb571b62b3b01dbb1b79
Brian Pepple <bdpepple@gmail.com>
2018-12-07 10:21:59 -0500

    Move the navbar burger javascript to a separate static file

---
+++ static/site/js/navbar.js
@@ -0,0 +1,8 @@
+(function() {
+	var burger = document.querySelector('.burger');
+	var nav = document.querySelector('#' + burger.dataset.target);
+	burger.addEventListener('click', function() {
+		burger.classList.toggle('is-active');
+		nav.classList.toggle('is-active');
+	});
+})();
