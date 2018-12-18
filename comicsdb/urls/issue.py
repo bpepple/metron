a9f8a888fce2cf768f396270f306ec4093b62c32
Brian Pepple <bdpepple@gmail.com>
2018-12-18 15:46:02 -0500

    Remove the Credits view
    * No longer needed since the Issue views use an inline formset
      for the credits.

--- comicsdb/urls/issue.py
+++ comicsdb/urls/issue.py
@@ -2,7 +2,6 @@ from django.urls import path, re_path
 
 from comicsdb.views.issue import (IssueList, IssueDetail, SearchIssueList,
                                   IssueCreate, IssueUpdate, IssueDelete)
-from comicsdb.views.credits import CreditsCreate
 from comicsdb.views.variant import VariantCreate
 
 
@@ -13,7 +12,6 @@ urlpatterns = [
     path('<slug:slug>/', IssueDetail.as_view(), name='detail'),
     path('<slug:slug>/update/', IssueUpdate.as_view(), name='update'),
     path('<slug:slug>/delete/', IssueDelete.as_view(), name='delete'),
-    path('<slug:slug>/add/credit/', CreditsCreate.as_view(), name='credit'),
     path('<slug:slug>/add/variant/', VariantCreate.as_view(), name='variant'),
     re_path(r'^search/?$', SearchIssueList.as_view(), name='search')
 ]
