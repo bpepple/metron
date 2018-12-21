25bf21f82f36cfbf22fd47d4bd2edb2db947ca94
Brian Pepple <bdpepple@gmail.com>
2018-12-21 09:41:48 -0500

    Use the postgresql UnaccentExtension() for the Creator name search

--- metron/settings.py
+++ metron/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
+    'django.contrib.postgres',
     'django.contrib.staticfiles',
     'django.contrib.sites',
     'django.contrib.sitemaps',
