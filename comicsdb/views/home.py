7c2b8c9d559e45986fc21be5911826ce64b2235c
Brian Pepple <bdpepple@gmail.com>
2018-12-25 11:22:24 -0500

    Add the recently edited issues to the home page

--- comicsdb/views/home.py
+++ comicsdb/views/home.py
@@ -7,8 +7,6 @@ from comicsdb.models import (Publisher, Series, Issue, Character,
 class HomePageView(TemplateView):
     template_name = 'comicsdb/home.html'
 
-#     TODO: Need to implement a better solution in the future
-#           but for now this should be fine.
     def get_context_data(self, **kwargs):
         context = super(HomePageView, self).get_context_data(**kwargs)
         context['publisher'] = Publisher.objects.count()
@@ -18,5 +16,11 @@ class HomePageView(TemplateView):
         context['creator'] = Creator.objects.count()
         context['team'] = Team.objects.count()
         context['arc'] = Arc.objects.count()
+        context['recent'] = (
+            Issue.objects
+            .prefetch_related('series')
+            .order_by('-modified')
+            .all()[:10]
+        )
 
         return context
