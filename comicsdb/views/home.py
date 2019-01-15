957779feb89af9b27f4b96559e113be1f80fdc52
Brian Pepple <bdpepple@gmail.com>
2019-01-15 18:02:13 -0500

    Show this week's issues on the home page.

--- comicsdb/views/home.py
+++ comicsdb/views/home.py
@@ -1,7 +1,13 @@
+from datetime import date
+
 from django.views.generic import TemplateView
 
 from comicsdb.models import (Publisher, Series, Issue, Character,
                              Creator, Team, Arc)
+
+
+current_week = date.today().isocalendar()[1]
+current_year = date.today().year
 
 
 class HomePageView(TemplateView):
@@ -22,5 +28,12 @@ class HomePageView(TemplateView):
             .order_by('-modified')
             .all()[:10]
         )
+        context['new'] = (
+            Issue.objects
+            .filter(store_date__week=current_week)
+            .filter(store_date__year=current_year)
+            .prefetch_related('series')
+        )
+        context
 
         return context
