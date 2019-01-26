68904d421c762657c6e28c0b9e51f5b1a9b33b18
Brian Pepple <bdpepple@gmail.com>
2019-01-26 16:46:58 -0500

    Fix creator urls

--- comicsdb/urls/creator.py
+++ comicsdb/urls/creator.py
@@ -11,9 +11,9 @@ urlpatterns = [
     path('create/', CreatorCreate.as_view(), name='create'),
     path('', CreatorList.as_view(), name='list'),
     path('<slug:slug>/', CreatorDetail.as_view(), name='detail'),
-    path('<slug:creator>/<slug:series>/',
-         CreatorSeriesList.as_view(), name='series'),
     path('<slug:slug>/update/', CreatorUpdate.as_view(), name='update'),
     path('<slug:slug>/delete/', CreatorDelete.as_view(), name='delete'),
+    path('<slug:creator>/<slug:series>/',
+         CreatorSeriesList.as_view(), name='series'),
     re_path(r'^search/?$', SearchCreatorList.as_view(), name='search')
 ]
