23b4fb39bda1cc039937b06e2268123b044b678b
Brian Pepple <bdpepple@gmail.com>
2019-02-11 16:25:47 -0500

    Add django-filters to project settings

--- metron/settings.py
+++ metron/settings.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.humanize',
     'django.forms',
     'rest_framework',
+    'django_filters',
     'widget_tweaks',
     'sorl.thumbnail',
     'comicsdb',
@@ -149,6 +150,7 @@ REST_FRAMEWORK = {
     'DEFAULT_AUTHENTICATION_CLASSES': (
         'rest_framework.authentication.SessionAuthentication',
     ),
+    'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
     'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
     'PAGE_SIZE': 28
 }
