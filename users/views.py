cfd49d91ee2cfe7ca0c5fb73fbc2268867ad96ac
Brian Pepple <bdpepple@gmail.com>
2019-04-22 12:26:39 -0400

    Enable user signups.

--- users/views.py
+++ users/views.py
@@ -1,11 +1,10 @@
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
-from django.contrib.auth.mixins import LoginRequiredMixin
 
 from .forms import CustomUserCreationForm
 
 
-class SignUp(LoginRequiredMixin, CreateView):
+class SignUp(CreateView):
     form_class = CustomUserCreationForm
     success_url = reverse_lazy('login')
     template_name = 'users/signup.html'
