1c96b1c3025beb662f3f90e7231c4abb7db1ff34
Brian Pepple <bdpepple@gmail.com>
2019-04-25 10:01:37 -0400

    Update CustomUserAdmin to use email_confirmed field

--- users/admin.py
+++ users/admin.py
@@ -10,4 +10,18 @@ class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['email', 'username', ]
+    list_display = ('username', 'email', 'is_active', 'email_confirmed')
+    list_filter = ('is_staff', 'is_superuser', 'is_active',
+                   'email_confirmed', 'groups')
+    fieldsets = (
+        (None, {'fields': ('username', 'password')}),
+        ('Personal info', {'fields': ('first_name', 'last_name', 'email')}),
+        ('Permissions', {'fields': ('is_active', 'is_staff', 'is_superuser',
+                                    'email_confirmed', 'groups', 'user_permissions')}),
+    )
+    add_fieldsets = (
+        (None, {
+            'classes': ('wide',),
+            'fields': ('username', 'email', 'password1', 'password2')}
+         ),
+    )
