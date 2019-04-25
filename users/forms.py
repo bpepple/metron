7ee822c07e52c21734ffe7211bfa516ee2039989
Brian Pepple <bdpepple@gmail.com>
2019-04-25 09:06:27 -0400

    Make User creation form require an email address

--- users/forms.py
+++ users/forms.py
@@ -1,9 +1,14 @@
 from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.forms import EmailField
 
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
+
+    email = EmailField(max_length=254,
+                       help_text='Required. Enter a valid email address.',
+                       required=True)
 
     class Meta(UserCreationForm):
         model = CustomUser
