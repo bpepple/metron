554316dbdb4c18983b5448b86df32a5213397838
Brian Pepple <bdpepple@gmail.com>
2019-05-13 16:47:33 -0400

    Use Basic Authentication for REST api

--- metron/settings.py
+++ metron/settings.py
@@ -148,6 +148,7 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.IsAuthenticated',
     ),
     'DEFAULT_AUTHENTICATION_CLASSES': (
+        'rest_framework.authentication.BasicAuthentication',
         'rest_framework.authentication.SessionAuthentication',
     ),
     'DEFAULT_FILTER_BACKENDS': ('django_filters.rest_framework.DjangoFilterBackend',),
