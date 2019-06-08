9a41102f48567635e7b3cc54989f24c37b960c03
Brian Pepple <bdpepple@gmail.com>
2019-06-08 09:30:09 -0400

    Refactor the context_data for SeriesDetail
    * Check for more than one series with same name for next prev navigation

--- comicsdb/views/series.py
+++ comicsdb/views/series.py
@@ -1,16 +1,16 @@
-from functools import reduce
 import operator
+from functools import reduce
 
-from django.contrib.auth.mixins import LoginRequiredMixin, PermissionRequiredMixin
+from django.contrib.auth.mixins import (LoginRequiredMixin,
+                                        PermissionRequiredMixin)
 from django.db.models import Q
 from django.shortcuts import get_object_or_404
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView
-from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import DetailView, ListView
+from django.views.generic.edit import CreateView, DeleteView, UpdateView
 
 from comicsdb.forms.series import SeriesForm
-from comicsdb.models import Series, Issue
-
+from comicsdb.models import Issue, Series
 
 PAGINATE = 28
 
@@ -44,25 +44,49 @@ class SeriesDetail(DetailView):
     def get_context_data(self, **kwargs):
         context = super(SeriesDetail, self).get_context_data(**kwargs)
         series = self.get_object()
-        try:
-            next_series = (
-                Series.objects
-                .order_by('name')
-                .filter(name__gt=series.name)
-                .first()
-            )
-        except:
-            next_series = None
 
-        try:
-            previous_series = (
-                Series.objects
-                .order_by('name')
-                .filter(name__lt=series.name)
-                .last()
-            )
-        except:
-            previous_series = None
+        # Set the initial value for the navigation variables
+        next_series = None
+        previous_series = None
+
+        # Create the base queryset with all the series.
+        qs = Series.objects.all().order_by('name', 'year_began')
+
+        # Determine if there is more than 1 series with the same name
+        series_count = qs.filter(name__gte=series.name).count()
+
+        # If there is more than one series with the same name
+        # let's attempt to get the next and previous items
+        if series_count > 1:
+            try:
+                next_series = (
+                    qs
+                    .filter(name=series.name, year_began__gt=series.year_began)
+                    .first()
+                )
+            except:
+                next_series = None
+
+            try:
+                previous_series = (
+                    qs
+                    .filter(name=series.name, year_began__lt=series.year_began)
+                    .last()
+                )
+            except:
+                previous_series = None
+
+        if not next_series:
+            try:
+                next_series = qs.filter(name__gt=series.name).first()
+            except:
+                next_series = None
+
+        if not previous_series:
+            try:
+                previous_series = qs.filter(name__lt=series.name).last()
+            except:
+                previous_series = None
 
         context['navigation'] = {
             'next_series': next_series,
