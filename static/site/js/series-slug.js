b7eb37d298005304bd8d1a0c2173d6b4347d0796
Brian Pepple <bdpepple@gmail.com>
2019-06-08 16:26:43 -0400

    Use double quotes instead of single quote

--- static/site/js/series-slug.js
+++ static/site/js/series-slug.js
@@ -2,7 +2,7 @@ document.getElementById("id_name").onkeyup = function() {
     var e = document.getElementById("id_slug");
     var seriesName = document.getElementById("id_name").value;
     var seriesYear = document.getElementById("id_year_began").value;
-    var res = seriesName + ' ' + seriesYear;
+    var res = seriesName + " " + seriesYear;
     if (!e._changed) { e.value = URLify(res, 255, false); }
 }
 
@@ -10,6 +10,6 @@ document.getElementById("id_year_began").onkeyup = function() {
     var e = document.getElementById("id_slug");
     var seriesName = document.getElementById("id_name").value;
     var seriesYear = document.getElementById("id_year_began").value;
-    var res = seriesName + ' ' + seriesYear;
+    var res = seriesName + " " + seriesYear;
     if (!e._changed) { e.value = URLify(res, 255, false); }
 }
