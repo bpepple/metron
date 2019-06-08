9ae176e5a8e0b61c098153ae16f3be38bbc6f857
Brian Pepple <bdpepple@gmail.com>
2019-06-08 14:59:37 -0400

    Fix series view tests

--- comicsdb/tests/test_series_views.py
+++ comicsdb/tests/test_series_views.py
@@ -1,8 +1,8 @@
 from django.test import TestCase
 from django.urls import reverse
 
-from comicsdb.models import Series, Publisher, SeriesType
-
+from comicsdb.models import Publisher, Series, SeriesType
+from users.models import CustomUser
 
 HTML_OK_CODE = 200
 
@@ -11,11 +11,28 @@ PAGINATE_DEFAULT_VAL = 28
 PAGINATE_DIFF_VAL = (PAGINATE_TEST_VAL - PAGINATE_DEFAULT_VAL)
 
 
-class SeriesSearchViewsTest(TestCase):
+class TestCaseBase(TestCase):
+
+    def _create_user(self):
+        user = CustomUser.objects.create(
+            username='brian', email='brian@test.com')
+        user.set_password('1234')
+        user.save()
+
+        return user
+
+    def _client_login(self):
+        self.client.login(username='brian', password='1234')
+
+
+class SeriesSearchViewsTest(TestCaseBase):
 
     @classmethod
     def setUpTestData(cls):
-        cls.publisher = Publisher.objects.create(name='DC', slug='dc')
+        user = cls._create_user(cls)
+
+        cls.publisher = Publisher.objects.create(
+            name='DC', slug='dc', edited_by=user)
         series_type = SeriesType.objects.create(name='Ongoing Series')
         for pub_num in range(PAGINATE_TEST_VAL):
             Series.objects.create(name=f'Series {pub_num}',
@@ -23,7 +40,11 @@ class SeriesSearchViewsTest(TestCase):
                                   sort_name=f'Series {pub_num}',
                                   year_began=2018,
                                   publisher=cls.publisher,
-                                  series_type=series_type)
+                                  series_type=series_type,
+                                  edited_by=user)
+
+    def setUp(self):
+        self._client_login()
 
     def test_view_url_exists_at_desired_location(self):
         resp = self.client.get('/series/search')
@@ -56,11 +77,14 @@ class SeriesSearchViewsTest(TestCase):
             len(resp.context['series_list']) == PAGINATE_DIFF_VAL)
 
 
-class SeriesListViewTest(TestCase):
+class SeriesListViewTest(TestCaseBase):
 
     @classmethod
     def setUpTestData(cls):
-        publisher = Publisher.objects.create(name='DC', slug='dc')
+        user = cls._create_user(cls)
+
+        publisher = Publisher.objects.create(
+            name='DC', slug='dc', edited_by=user)
         series_type = SeriesType.objects.create(name='Ongoing Series')
         for pub_num in range(PAGINATE_TEST_VAL):
             Series.objects.create(name=f'Series {pub_num}',
@@ -68,7 +92,11 @@ class SeriesListViewTest(TestCase):
                                   sort_name=f'Series {pub_num}',
                                   year_began=2018,
                                   publisher=publisher,
-                                  series_type=series_type)
+                                  series_type=series_type,
+                                  edited_by=user)
+
+    def setUp(self):
+        self._client_login()
 
     def test_view_url_exists_at_desired_location(self):
         resp = self.client.get('/series/')
