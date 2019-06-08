fbd4ca54bfcc37552ee2c58577f36159f0d7b031
Brian Pepple <bdpepple@gmail.com>
2019-06-08 17:58:20 -0400

    Move close Modals function before it's called

--- static/site/js/image-modal.js
+++ static/site/js/image-modal.js
@@ -7,6 +7,13 @@ document.addEventListener("DOMContentLoaded", function () {
     return Array.prototype.slice.call(document.querySelectorAll(selector), 0);
   }
 
+  function closeModals() {
+    rootEl.classList.remove("is-clipped");
+    $modals.forEach(function ($el) {
+      $el.classList.remove("is-active");
+    });
+  }
+
   // Modals
 
   var rootEl = document.documentElement;
@@ -39,11 +46,4 @@ document.addEventListener("DOMContentLoaded", function () {
       closeModals();
     }
   });
-
-  function closeModals() {
-    rootEl.classList.remove("is-clipped");
-    $modals.forEach(function ($el) {
-      $el.classList.remove("is-active");
-    });
-  }
 });
