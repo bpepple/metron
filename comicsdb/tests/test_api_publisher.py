58b38863fe50e403957de2370e87920a652efa7f
Brian Pepple <bdpepple@gmail.com>
2019-06-08 13:58:36 -0400

    Fix publisher api tests

--- comicsdb/tests/test_api_publisher.py
+++ comicsdb/tests/test_api_publisher.py
@@ -3,7 +3,7 @@ from django.urls import reverse
 from django.utils import timezone
 from rest_framework import status
 
-from comicsdb.models import Publisher, Series, SeriesType, Issue
+from comicsdb.models import Issue, Publisher, Series, SeriesType
 from comicsdb.serializers import PublisherSerializer, SeriesListSerializer
 from users.models import CustomUser
 
@@ -26,10 +26,11 @@ class GetAllPublisherTest(TestCaseBase):
 
     @classmethod
     def setUpTestData(cls):
-        cls._create_user(cls)
+        user = cls._create_user(cls)
 
-        Publisher.objects.create(name='DC Comics', slug='dc-comics')
-        Publisher.objects.create(name='Marvel', slug='marvel')
+        Publisher.objects.create(
+            name='DC Comics', slug='dc-comics', edited_by=user)
+        Publisher.objects.create(name='Marvel', slug='marvel', edited_by=user)
 
     def setUp(self):
         self._client_login()
@@ -41,27 +42,29 @@ class GetAllPublisherTest(TestCaseBase):
     def test_unauthorized_view_url(self):
         self.client.logout()
         resp = self.client.get(reverse('api:publisher-list'))
-        self.assertEqual(resp.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(resp.status_code, status.HTTP_401_UNAUTHORIZED)
 
 
 class GetSinglePublisherTest(TestCaseBase):
 
     @classmethod
     def setUpTestData(cls):
-        cls._create_user(cls)
+        user = cls._create_user(cls)
 
-        cls.dc = Publisher.objects.create(name='DC Comics', slug='dc-comics')
-        cls.marvel = Publisher.objects.create(name='Marvel', slug='marvel')
+        cls.dc = Publisher.objects.create(
+            name='DC Comics', slug='dc-comics', edited_by=user)
+        cls.marvel = Publisher.objects.create(
+            name='Marvel', slug='marvel', edited_by=user)
 
         series_type_obj = SeriesType.objects.create(name='Cancelled')
         cls.series_obj = Series.objects.create(name='Final Crisis', slug='final-crisis',
                                                publisher=cls.dc, year_began=1939,
-                                               series_type=series_type_obj)
+                                               series_type=series_type_obj, edited_by=user)
         Issue.objects.create(series=cls.series_obj,
                              number='1',
                              slug='final-crisis-1',
                              image='issue/test.jpg',
-                             cover_date=timezone.now().date())
+                             cover_date=timezone.now().date(), edited_by=user)
 
     def setUp(self):
         self._client_login()
@@ -83,7 +86,7 @@ class GetSinglePublisherTest(TestCaseBase):
         self.client.logout()
         response = self.client.get(
             reverse('api:publisher-detail', kwargs={'pk': self.dc.pk}))
-        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
     def test_publisher_series_list_view(self):
         response = self.client.get(reverse('api:publisher-series-list',
