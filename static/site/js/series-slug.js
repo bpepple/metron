611bd7cf145be82b912add7de3cd405c4ae90c79
Brian Pepple <bdpepple@gmail.com>
2019-06-08 17:32:12 -0400

    Move duplicat js code into a function

--- static/site/js/series-slug.js
+++ static/site/js/series-slug.js
@@ -1,15 +1,11 @@
-document.getElementById("id_name").onkeyup = function() {
+function toSlug() {
     var e = document.getElementById("id_slug");
     var seriesName = document.getElementById("id_name").value;
     var seriesYear = document.getElementById("id_year_began").value;
     var res = seriesName + " " + seriesYear;
     if (!e._changed) { e.value = URLify(res, 255, false); }
-}
+  }
 
-document.getElementById("id_year_began").onkeyup = function() {
-    var e = document.getElementById("id_slug");
-    var seriesName = document.getElementById("id_name").value;
-    var seriesYear = document.getElementById("id_year_began").value;
-    var res = seriesName + " " + seriesYear;
-    if (!e._changed) { e.value = URLify(res, 255, false); }
-}
+document.getElementById("id_name").onkeyup = toSlug;
+
+document.getElementById("id_year_began").onkeyup = toSlug;
