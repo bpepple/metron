06d260b8cd9d7baeac5136bba32350830699e1f1
Brian Pepple <bdpepple@gmail.com>
2019-06-13 13:18:43 -0400

    Use Spaces custom domain

--- metron/settings/production.py
+++ metron/settings/production.py
@@ -30,5 +30,5 @@ AWS_DEFAULT_ACL = "public-read"
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
 STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
-STATIC_URL = "{}/{}/".format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
+STATIC_URL = f"{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
 STATIC_ROOT = config("STATIC_ROOT")
