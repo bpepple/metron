4b9b880ae61cdcff6db99b69e931b6126c94d086
Brian Pepple <bdpepple@gmail.com>
2019-06-13 12:43:45 -0400

    Include the static file directories

--- metron/settings/production.py
+++ metron/settings/production.py
@@ -29,5 +29,6 @@ AWS_DEFAULT_ACL = "public-read"
 
 STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
+STATICFILES_DIRS = (os.path.join(BASE_DIR, "static"),)
 STATIC_URL = "{}/{}/".format(AWS_S3_ENDPOINT_URL, AWS_LOCATION)
 STATIC_ROOT = config("STATIC_ROOT")
