353d1777da371ae7e01b20a3c5736e003154a5c5
Brian Pepple <bdpepple@gmail.com>
2019-07-01 12:34:15 -0400

    Add statistic for daily issues added

--- comicsdb/views/statistics.py
+++ comicsdb/views/statistics.py
@@ -1,7 +1,7 @@
 from datetime import datetime
 
 from django.db.models import Count
-from django.db.models.functions import TruncMonth
+from django.db.models.functions import TruncDate, TruncMonth
 from django.views.generic.base import TemplateView
 
 from comicsdb.models import Character, Creator, Issue, Publisher
@@ -21,9 +21,10 @@ class StatisticsView(TemplateView):
         for publisher in publishers:
             publisher_dict.update({publisher["name"]: publisher["num_issues"]})
 
-        # Monthly Issues Added Counts
+        # Dates used for issue counts querysets
         today = datetime.now()
         last_year = today.year - 1
+        last_month = today.month - 1
 
         # This should show the last 24 months probably need to refine the
         # queryset to show something more reasonable like 12 months.
@@ -38,6 +39,21 @@ class StatisticsView(TemplateView):
         for issue in issues:
             month_str = issue["month"].strftime("%b")
             issue_dict.update({month_str: issue["c"]})
+
+        # Daily added issue counts.
+        issues = (
+            Issue.objects.filter(
+                created_on__year=today.year, created_on__month__gte=last_month
+            )
+            .annotate(day=TruncDate("created_on"))
+            .values("day")
+            .annotate(c=Count("day"))
+            .order_by("day")[:30]
+        )
+        daily_issue_dict = {}
+        for issue in issues:
+            day_str = issue["day"].strftime("%m/%d")
+            daily_issue_dict.update({day_str: issue["c"]})
 
         # Monthly Creators Added
         creators = (
@@ -68,6 +84,7 @@ class StatisticsView(TemplateView):
         # Assign the context values
         context["publisher_counts"] = publisher_dict
         context["monthly_issues"] = issue_dict
+        context["daily_issue_count"] = daily_issue_dict
         context["monthly_creator"] = creator_dict
         context["monthly_character"] = character_dict
 
