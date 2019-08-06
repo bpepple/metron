2804ca54d00e6bc1645093e997e3856b5cac1136
Brian Pepple <bdpepple@gmail.com>
2019-08-06 14:18:03 -0400

    Add DRF default schema class
    * Refer to:
    
    https://www.django-rest-framework.org/community/3.10-announcement/#continuing-to-use-coreapi

--- metron/settings/common.py
+++ metron/settings/common.py
@@ -136,6 +136,7 @@ REST_FRAMEWORK = {
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
     "PAGE_SIZE": 28,
+    'DEFAULT_SCHEMA_CLASS': 'rest_framework.schemas.coreapi.AutoSchema',
 }
 
 
