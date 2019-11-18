1cc02ae23b5fe2614a78099fdcb301185acef67c
Brian Pepple <bdpepple@gmail.com>
2019-11-18 09:19:39 -0500

    Remove unnecessary script
    * django-simple-bulma provides a replacement.

--- static/site/js/navbar.js
+++
@@ -1,8 +0,0 @@
-(function() {
-	var burger = document.querySelector(".burger");
-	var nav = document.querySelector("#" + burger.dataset.target);
-	burger.addEventListener("click", function() {
-		burger.classList.toggle("is-active");
-		nav.classList.toggle("is-active");
-	});
-})();
