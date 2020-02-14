8662361c6667a9e012953e510e5b8f6480c7f607
Brian Pepple <bdpepple@gmail.com>
2020-02-14 15:55:00 -0500

    Refactor api_arc tests for pytest

---
+++ comicsdb/tests/conftest.py
@@ -0,0 +1,95 @@
+import uuid
+
+import pytest
+from django.utils import timezone
+
+from comicsdb.models import Arc, Publisher, SeriesType, Series, Issue
+from users.models import CustomUser
+
+
+@pytest.fixture
+def test_password():
+    return "strong*test#pass12345"
+
+
+@pytest.fixture
+def test_email():
+    return "test@example.com"
+
+
+@pytest.fixture
+def create_user(db, test_password, test_email):
+    def make_user(**kwargs):
+        kwargs["password"] = test_password
+        if "username" not in kwargs:
+            kwargs["username"] = str(uuid.uuid4())
+        if "email" not in kwargs:
+            kwargs["email"] = test_email
+        return CustomUser.objects.create(**kwargs)
+
+    return make_user
+
+
+@pytest.fixture()
+def auto_login_user(db, client, create_user, test_password):
+    def make_auto_login(user=None):
+        if user is None:
+            user = create_user()
+        client.login(username=user.username, password=test_password)
+        return client, user
+
+    return make_auto_login
+
+
+@pytest.fixture
+def api_client():
+    from rest_framework.test import APIClient
+
+    return APIClient()
+
+
+@pytest.fixture
+def api_client_with_credentials(db, create_user, api_client):
+    user = create_user()
+    api_client.force_authenticate(user=user)
+    yield api_client
+    api_client.force_authenticate(user=None)
+
+
+@pytest.fixture
+def arc_fixture(db, create_user):
+    user = create_user()
+    return Arc.objects.create(
+        name="World War Hulk", slug="world-war-hulk", edited_by=user
+    )
+
+
+@pytest.fixture
+def publisher_fixture(db, create_user):
+    user = create_user()
+    return Publisher.objects.create(name="DC Comics", slug="dc-comics", edited_by=user)
+
+
+@pytest.fixture
+def issue_fixture(db, create_user, publisher_fixture):
+    user = create_user()
+    series_type = SeriesType.objects.create(name="Cancelled")
+    series = Series.objects.create(
+        name="Final Crisis",
+        slug="final-crisis",
+        publisher=publisher_fixture,
+        volume="1",
+        year_began=1999,
+        series_type=series_type,
+        edited_by=user,
+    )
+    arc = Arc.objects.create(name="Final Crisis", slug="final-crisis", edited_by=user)
+    issue = Issue.objects.create(
+        series=series,
+        number="1",
+        slug="final-crisis-1",
+        cover_date=timezone.now().date(),
+        edited_by=user,
+    )
+    issue.arcs.add(arc)
+    return issue
--- comicsdb/tests/test_api_arc.py
+++ comicsdb/tests/test_api_arc.py
@@ -1,97 +1,54 @@
+import pytest
 from django.urls import reverse
-from django.utils import timezone
 from rest_framework import status
 
-from comicsdb.models import Arc, Issue, Publisher, Series, SeriesType
 from comicsdb.serializers import ArcSerializer, IssueListSerializer
 
-from .case_base import TestCaseBase
+
+@pytest.mark.django_db
+def test_api_arc_issue_list(api_client_with_credentials, issue_fixture):
+    response = api_client_with_credentials.get(
+        reverse("api:arc-issue-list", kwargs={"pk": issue_fixture.pk})
+    )
+    serializer = IssueListSerializer(issue_fixture)
+    assert response.data["count"] == 1
+    assert response.data["next"] is None
+    assert response.data["previous"] is None
+    assert response.data["results"][0] == serializer.data
+    assert response.status_code == status.HTTP_200_OK
 
 
-class GetAllArcsTest(TestCaseBase):
-    @classmethod
-    def setUpTestData(cls):
-        user = cls._create_user()
-
-        Arc.objects.create(name="World War Hulk", slug="world-war-hulk", edited_by=user)
-        Arc.objects.create(name="Final Crisis", slug="final-crisis", edited_by=user)
-
-    def setUp(self):
-        self._client_login()
-
-    def test_view_url_accessible_by_name(self):
-        resp = self.client.get(reverse("api:arc-list"))
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
-
-    def test_unauthorized_view_url(self):
-        self.client.logout()
-        resp = self.client.get(reverse("api:arc-list"))
-        self.assertEqual(resp.status_code, status.HTTP_401_UNAUTHORIZED)
+@pytest.mark.django_db
+def test_unauthorized_api_arc_detail(api_client, arc_fixture):
+    response = api_client.get(reverse("api:arc-detail", kwargs={"pk": arc_fixture.pk}))
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
 
 
-class GetSingleArcTest(TestCaseBase):
-    @classmethod
-    def setUpTestData(cls):
-        user = cls._create_user()
+@pytest.mark.django_db
+def test_authorized_api_arc_detail(api_client_with_credentials, arc_fixture):
+    response = api_client_with_credentials.get(
+        reverse("api:arc-detail", kwargs={"pk": arc_fixture.pk})
+    )
+    serializer = ArcSerializer(arc_fixture)
+    assert response.data == serializer.data
+    assert response.status_code == status.HTTP_200_OK
 
-        publisher_obj = Publisher.objects.create(
-            name="DC Comics", slug="dc-comics", edited_by=user
-        )
-        series_type_obj = SeriesType.objects.create(name="Cancelled")
-        series_obj = Series.objects.create(
-            name="Final Crisis",
-            slug="final-crisis",
-            publisher=publisher_obj,
-            volume="1",
-            year_began=1939,
-            series_type=series_type_obj,
-            edited_by=user,
-        )
-        cls.issue_obj = Issue.objects.create(
-            series=series_obj,
-            number="1",
-            slug="final-crisis-1",
-            cover_date=timezone.now().date(),
-            edited_by=user,
-        )
-        cls.hulk = Arc.objects.create(
-            name="World War Hulk", slug="world-war-hulk", edited_by=user
-        )
-        cls.crisis = Arc.objects.create(
-            name="Final Crisis", slug="final-crisis", edited_by=user
-        )
-        cls.issue_obj.arcs.add(cls.crisis)
 
-    def setUp(self):
-        self._client_login()
+@pytest.mark.django_db
+def test_get_invalid_api_arc_detail(api_client_with_credentials, arc_fixture):
+    response = api_client_with_credentials.get(
+        reverse("api:arc-detail", kwargs={"pk": "10"})
+    )
+    assert response.status_code == status.HTTP_404_NOT_FOUND
 
-    def test_get_valid_single_arc(self):
-        response = self.client.get(
-            reverse("api:arc-detail", kwargs={"pk": self.hulk.pk})
-        )
-        arc = Arc.objects.get(pk=self.hulk.pk)
-        serializer = ArcSerializer(arc)
-        self.assertEqual(response.data, serializer.data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
 
-    def test_get_invalid_single_arc(self):
-        response = self.client.get(reverse("api:arc-detail", kwargs={"pk": "10"}))
-        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+@pytest.mark.django_db
+def test_authorized_api_arc_list(api_client_with_credentials, arc_fixture):
+    response = api_client_with_credentials.get(reverse("api:arc-list"))
+    assert response.status_code == status.HTTP_200_OK
 
-    def test_unauthorized_view_url(self):
-        self.client.logout()
-        response = self.client.get(
-            reverse("api:arc-detail", kwargs={"pk": self.hulk.pk})
-        )
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-    def test_arc_issue_list_view(self):
-        response = self.client.get(
-            reverse("api:arc-issue-list", kwargs={"pk": self.crisis.pk})
-        )
-        serializer = IssueListSerializer(self.issue_obj)
-        self.assertEqual(response.data["count"], 1)
-        self.assertEqual(response.data["next"], None)
-        self.assertEqual(response.data["previous"], None)
-        self.assertEqual(response.data["results"][0], serializer.data)
-        self.assertEqual(response.status_code, status.HTTP_200_OK)
+@pytest.mark.django_db
+def test_unauthorized_api_arc_list_url(api_client, arc_fixture):
+    response = api_client.get(reverse("api:arc-list"))
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
