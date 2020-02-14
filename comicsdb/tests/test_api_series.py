2d3649b201dded2593b94ada8b9fc88d0bb1b4b2
Brian Pepple <bdpepple@gmail.com>
2020-02-14 17:54:30 -0500

    Refactor api series tests

--- comicsdb/tests/test_api_series.py
+++ comicsdb/tests/test_api_series.py
@@ -1,102 +1,35 @@
+import pytest
 from django.urls import reverse
-from django.utils import timezone
 from rest_framework import status
-from rest_framework.request import Request
-from rest_framework.test import APIRequestFactory
 
-from comicsdb.models import Issue, Publisher, Series, SeriesType
 from comicsdb.serializers import SeriesSerializer
 
-from .case_base import TestCaseBase
+
+@pytest.mark.django_db
+def test_api_series_list_authorized_view(api_client_with_credentials):
+    resp = api_client_with_credentials.get(reverse("api:series-list"))
+    assert resp.status_code == status.HTTP_200_OK
 
 
-class GetAllSeriesTest(TestCaseBase):
-    @classmethod
-    def setUpTestData(cls):
-        user = cls._create_user()
-
-        publisher_obj = Publisher.objects.create(
-            name="DC Comics", slug="dc-comics", edited_by=user
-        )
-        series_type_obj = SeriesType.objects.create(name="Cancelled")
-        Series.objects.create(
-            name="Superman",
-            slug="superman",
-            publisher=publisher_obj,
-            volume="1",
-            year_began=1939,
-            series_type=series_type_obj,
-            edited_by=user,
-        )
-        Series.objects.create(
-            name="Batman",
-            slug="batman",
-            publisher=publisher_obj,
-            volume="1",
-            year_began=1940,
-            series_type=series_type_obj,
-            edited_by=user,
-        )
-
-    def setUp(self):
-        self._client_login()
-
-    def test_view_url_accessible_by_name(self):
-        resp = self.client.get(reverse("api:series-list"))
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
-
-    def test_unauthorized_view_url(self):
-        self.client.logout()
-        resp = self.client.get(reverse("api:series-list"))
-        self.assertEqual(resp.status_code, status.HTTP_401_UNAUTHORIZED)
+@pytest.mark.django_db
+def test_api_series_list_unauthorized_view(api_client):
+    resp = api_client.get(reverse("api:series-list"))
+    assert resp.status_code == status.HTTP_401_UNAUTHORIZED
 
 
-class GetSingleSeriesTest(TestCaseBase):
-    @classmethod
-    def setUpTestData(cls):
-        user = cls._create_user()
+@pytest.mark.django_db
+def test_authorized_api_series_detail_view(api_client_with_credentials, issue_fixture):
+    resp = api_client_with_credentials.get(
+        reverse("api:series-detail", kwargs={"pk": issue_fixture.pk})
+    )
+    serializer = SeriesSerializer(issue_fixture.series)
+    assert resp.data == serializer.data
+    assert resp.status_code == status.HTTP_200_OK
 
-        factory = APIRequestFactory()
-        request = factory.get("/")
 
-        cls.serializer_context = {"request": Request(request)}
-
-        publisher_obj = Publisher.objects.create(
-            name="Marvel", slug="marvel", edited_by=user
-        )
-        series_type_obj = SeriesType.objects.create(name="Cancelled")
-        cls.thor = Series.objects.create(
-            name="The Mighty Thor",
-            slug="the-mighty-thor",
-            publisher=publisher_obj,
-            volume="1",
-            year_began=1965,
-            series_type=series_type_obj,
-            edited_by=user,
-        )
-        Issue.objects.create(
-            slug="thor-1",
-            cover_date=timezone.now().date(),
-            number="1",
-            series=cls.thor,
-            edited_by=user,
-        )
-
-    def setUp(self):
-        self._client_login()
-
-    def test_get_valid_single_issue(self):
-        resp = self.client.get(
-            reverse("api:series-detail", kwargs={"pk": self.thor.pk})
-        )
-        series = Series.objects.get(pk=self.thor.pk)
-        serializer = SeriesSerializer(series, context=self.serializer_context)
-        self.assertEqual(resp.data, serializer.data)
-        self.assertEqual(resp.status_code, status.HTTP_200_OK)
-
-    def test_unauthorized_view_url(self):
-        self.client.logout()
-        response = self.client.get(
-            reverse("api:series-detail", kwargs={"pk": self.thor.pk})
-        )
-        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+@pytest.mark.django_db
+def test_unauthorized_api_series_detail_view(api_client, series_fixture):
+    response = api_client.get(
+        reverse("api:series-detail", kwargs={"pk": series_fixture.pk})
+    )
+    assert response.status_code == status.HTTP_401_UNAUTHORIZED
