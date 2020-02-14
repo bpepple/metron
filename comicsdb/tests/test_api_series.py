cd621d8e0e4ee4b0cc582cfc4156e1098fb61689
Brian Pepple <bdpepple@gmail.com>
2020-02-14 17:56:06 -0500

    Rename a couple of tests

--- comicsdb/tests/test_api_series.py
+++ comicsdb/tests/test_api_series.py
@@ -6,13 +6,13 @@ from comicsdb.serializers import SeriesSerializer
 
 
 @pytest.mark.django_db
-def test_api_series_list_authorized_view(api_client_with_credentials):
+def test_authorized_api_series_list_view(api_client_with_credentials):
     resp = api_client_with_credentials.get(reverse("api:series-list"))
     assert resp.status_code == status.HTTP_200_OK
 
 
 @pytest.mark.django_db
-def test_api_series_list_unauthorized_view(api_client):
+def test_unauthorized_api_series_list_view(api_client):
     resp = api_client.get(reverse("api:series-list"))
     assert resp.status_code == status.HTTP_401_UNAUTHORIZED
 
