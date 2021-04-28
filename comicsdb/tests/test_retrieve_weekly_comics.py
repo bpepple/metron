7184402a9d451a84bb6e3aaa71f1616541dd7abc
Brian Pepple <bdpepple@gmail.com>
2021-04-27 23:47:06 -0400

    Minor fixes to tests

--- comicsdb/tests/test_retrieve_weekly_comics.py
+++ comicsdb/tests/test_retrieve_weekly_comics.py
@@ -1,13 +1,14 @@
+import datetime
+
 from comicsdb.management.commands.retrieve_weekly_comics import (
     determine_cover_date,
     format_string_to_date,
 )
-import datetime
 
 from .case_base import TestCaseBase
 
 
-class YourTestClass(TestCaseBase):
+class TestCommands(TestCaseBase):
     def test_release_date(self):
         test_str = "2021-04-12"
         expected_result = datetime.date(2021, 4, 12)
@@ -17,6 +18,5 @@ class YourTestClass(TestCaseBase):
     def test_determine_cover_date(self):
         test_date = datetime.date(2021, 4, 12)
         expected_result = datetime.date(2021, 6, 1)
-
         result = determine_cover_date(test_date)
         self.assertEqual(expected_result, result)
