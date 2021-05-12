c8a99baeea00bcfc9ff6e6965a871feb98bb3fd8
Brian Pepple <bdpepple@gmail.com>
2021-05-12 16:08:11 -0400

    Initial work on navbar change
    Reworking for user profile

--- users/urls.py
+++ users/urls.py
@@ -15,5 +15,5 @@ urlpatterns = [
         views.activate,
         name="activate",
     ),
-    path("<int:pk>/", views.UserProfile.as_view(), name="detail"),
+    path("<int:pk>/", views.UserProfile.as_view(), name="user-detail"),
 ]
