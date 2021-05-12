45a3a140bb06127ffd4846e4ed548a23f0cacf22
Brian Pepple <bdpepple@gmail.com>
2021-05-12 10:38:54 -0400

    Add profile image & created_on fields

--- users/models.py
+++ users/models.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.models import AbstractUser
 from django.db import models
+from sorl.thumbnail import ImageField
 
 
 class CustomUser(AbstractUser):
     email_confirmed = models.BooleanField(default=False)
+    image = ImageField(upload_to="user/", blank=True)
+    created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
-        return self.email
+        return self.username
