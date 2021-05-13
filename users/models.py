3940269b5ce1a7db3586239ebfc9d6919934d7a8
Brian Pepple <bdpepple@gmail.com>
2021-05-13 15:24:12 -0400

    Remove user created_on field

--- users/models.py
+++ users/models.py
@@ -6,7 +6,6 @@ from sorl.thumbnail import ImageField
 class CustomUser(AbstractUser):
     email_confirmed = models.BooleanField(default=False)
     image = ImageField(upload_to="user/", blank=True)
-    created_on = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.username
