72a9b5dc1c1fd0ea0b9f52b8c89293e2bb6ff0bc
Brian Pepple <bdpepple@gmail.com>
2021-05-31 09:16:08 -0400

    Add more fields to issue filter
    * Added store_date & publisher fields

--- comicsdb/filters/issue.py
+++ comicsdb/filters/issue.py
@@ -1,22 +1,25 @@
 import django_filters
-
 from comicsdb.models import Issue
 
 
 class IssueFilter(django_filters.FilterSet):
-    series_name = django_filters.CharFilter(
-        field_name="series__name", lookup_expr="icontains"
-    )
-    series_volume = django_filters.NumberFilter(
-        field_name="series__volume", lookup_expr="exact"
-    )
     cover_year = django_filters.NumberFilter(
         field_name="cover_date", lookup_expr="year"
     )
     cover_month = django_filters.NumberFilter(
         field_name="cover_date", lookup_expr="month"
     )
+    publisher = django_filters.filters.CharFilter(
+        field_name="series__publisher__name", lookup_expr="icontains"
+    )
+    series_name = django_filters.CharFilter(
+        field_name="series__name", lookup_expr="icontains"
+    )
+    series_volume = django_filters.NumberFilter(
+        field_name="series__volume", lookup_expr="exact"
+    )
+    store_date_range = django_filters.filters.DateFromToRangeFilter("store_date")
 
     class Meta:
         model = Issue
-        fields = ["series_name", "series_volume", "number", "cover_year", "cover_month"]
+        fields = ["number", "store_date"]
