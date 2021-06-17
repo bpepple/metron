48f50b10310912157eccb7e2641667264165c767
Brian Pepple <bdpepple@gmail.com>
2021-06-16 21:45:35 -0400

    Use the bulma css on the profile form. Fixes #59

--- users/forms.py
+++ users/forms.py
@@ -1,11 +1,10 @@
-from django.contrib.auth.forms import UserCreationForm, UserChangeForm
-from django.forms import EmailField
+from django.contrib.auth.forms import UserChangeForm, UserCreationForm
+from django.forms import ClearableFileInput, EmailField, EmailInput, TextInput
 
 from .models import CustomUser
 
 
 class CustomUserCreationForm(UserCreationForm):
-
     email = EmailField(
         max_length=254,
         help_text="Required. Enter a valid email address.",
@@ -18,6 +17,20 @@ class CustomUserCreationForm(UserCreationForm):
 
 
 class CustomUserChangeForm(UserChangeForm):
+    email = EmailField(
+        max_length=254,
+        help_text="Required. Enter a valid email address.",
+        required=True,
+        widget=EmailInput(attrs={"class": "input"}),
+        error_messages={"required": "Please provide valid email."},
+    )
+
     class Meta:
         model = CustomUser
         fields = ("username", "first_name", "last_name", "email", "image")
+        widgets = {
+            "username": TextInput(attrs={"class": "input"}),
+            "first_name": TextInput(attrs={"class": "input"}),
+            "last_name": TextInput(attrs={"class": "input"}),
+            "image": ClearableFileInput(),
+        }
