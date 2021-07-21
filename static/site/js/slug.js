053d84b725205b6f1192f7f127f6e869b96a41a0
Brian Pepple <bdpepple@gmail.com>
2021-07-21 15:35:41 -0400

    Drop unused slug javascript

--- static/site/js/slug.js
+++
@@ -1,4 +0,0 @@
-document.getElementById("id_name").onkeyup = function() {
-    var e = document.getElementById("id_slug");
-    if (!e._changed) { e.value = URLify(document.getElementById("id_name").value, 255, false); }
-}
