9ae5750beb78b17b8be24d089fdb274c4d63b30a
Brian Pepple <bdpepple@gmail.com>
2021-07-29 10:09:38 -0400

    Use correct parameter

--- metron/tests.py
+++ metron/tests.py
@@ -7,7 +7,7 @@ HTML_OK_CODE = 200
 
 class TestCaseBase(TestCase):
     @classmethod
-    def _create_user(self):
+    def _create_user(cls):
         user = CustomUser.objects.create(username="brian", email="brian@test.com")
         user.set_password("1234")
         user.save()
