349d828d32449c6cc3c2380478722225df2bdfc2
Brian Pepple <bdpepple@gmail.com>
2021-09-28 11:54:10 -0400

    Change drf pagination page size to 100

--- metron/settings.py
+++ metron/settings.py
@@ -157,7 +157,7 @@ REST_FRAMEWORK = {
     "DEFAULT_THROTTLE_RATES": {"user": "25/minute"},
     "DEFAULT_FILTER_BACKENDS": ("django_filters.rest_framework.DjangoFilterBackend",),
     "DEFAULT_PAGINATION_CLASS": "rest_framework.pagination.PageNumberPagination",
-    "PAGE_SIZE": 28,
+    "PAGE_SIZE": 100,
     "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
 }
 
