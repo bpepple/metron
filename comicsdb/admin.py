f727d389681957ff54c68bc33f78a3eca0377cb7
Brian Pepple <bdpepple@gmail.com>
2021-11-21 10:46:04 -0500

    Add series field to Series admin

--- comicsdb/admin.py
+++ comicsdb/admin.py
@@ -3,6 +3,9 @@ from typing import Any, Optional
 
 from django.contrib import admin
 from django.db.models.query import QuerySet
+from django.forms.models import ModelForm
+from django.forms.widgets import Select, Textarea, TextInput
+from searchableselect.widgets import SearchableSelect
 from simple_history.admin import SimpleHistoryAdmin
 from sorl.thumbnail.admin import AdminImageMixin
 
@@ -193,8 +196,35 @@ class RoleAdmin(admin.ModelAdmin):
     fields = ("name", "notes", "order", "modified")
 
 
+class SeriesAdminForm(ModelForm):
+    class Meta:
+        model = Series
+        exclude = ()
+        widgets = {
+            "name": TextInput(attrs={"class": "input"}),
+            "sort_name": TextInput(attrs={"class": "input"}),
+            "volume": TextInput(attrs={"class": "input"}),
+            "year_began": TextInput(attrs={"class": "input"}),
+            "year_end": TextInput(attrs={"class": "input"}),
+            "series_type": Select(),
+            "series": SearchableSelect(
+                model="comicsdb.Series", search_field="name", many=True, limit=200
+            ),
+            "publisher": Select(),
+            "desc": Textarea(attrs={"class": "textarea"}),
+        }
+        help_texts = {
+            "sort_name": """Most of the time it will be the same as the series name,
+            but if the title starts with an article like 'The' it might be remove so
+            that it is listed with like named series.""",
+            "year_end": "Leave blank if a One-Shot, Annual, or Ongoing Series.",
+            "series": "Associate a series with another. For example an annual with it's primary series.",
+        }
+
+
 @admin.register(Series)
 class SeriesAdmin(SimpleHistoryAdmin):
+    form = SeriesAdminForm
     search_fields = ("name",)
     list_display = ("name", "year_began")
     list_filter = ("modified", "publisher")
@@ -208,6 +238,7 @@ class SeriesAdmin(SimpleHistoryAdmin):
         "year_began",
         "year_end",
         "series_type",
+        "series",
         "desc",
         "edited_by",
     )
