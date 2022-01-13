f37130b61c34547634a3457bbaa8cd004b566832
Brian Pepple <bdpepple@gmail.com>
2022-01-12 21:54:09 -0500

    Rewrite command_import_shortboxed

--- comicsdb/tests/test_command_import_shortboxed.py
+++ comicsdb/tests/test_command_import_shortboxed.py
@@ -1,18 +1,17 @@
 import datetime
 
 from comicsdb.management.commands.import_shortboxed import determine_cover_date
-from users.tests.case_base import TestCaseBase
 
 
-class TestSandBoxedCommands(TestCaseBase):
-    def test_determine_marvel_cover_date(self):
-        test_date = datetime.date(2021, 4, 12)
-        expected_result = datetime.date(2021, 6, 1)
-        result = determine_cover_date(test_date, "marvel comics")
-        self.assertEqual(expected_result, result)
+def test_determine_marvel_cover_date():
+    test_date = datetime.date(2021, 4, 12)
+    expected_result = datetime.date(2021, 6, 1)
+    result = determine_cover_date(test_date, "marvel comics")
+    assert expected_result == result
 
-    def test_determine_image_cover_date(self):
-        test_date = datetime.date(2021, 4, 12)
-        expected_result = datetime.date(2021, 4, 1)
-        result = determine_cover_date(test_date, "IMAGE COMICS")
-        self.assertEqual(expected_result, result)
+
+def test_determine_image_cover_date():
+    test_date = datetime.date(2021, 4, 12)
+    expected_result = datetime.date(2021, 4, 1)
+    result = determine_cover_date(test_date, "IMAGE COMICS")
+    assert expected_result == result
