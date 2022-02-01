c2097777b83accda51215f7e378958c24f75d97d
Brian Pepple <bdpepple@gmail.com>
2022-02-01 11:19:19 -0500

    Add issue filters for sku & upc codes
    - Also added labels for some of the filters

--- comicsdb/filters/issue.py
+++ comicsdb/filters/issue.py
@@ -4,24 +4,36 @@ from comicsdb.models import Issue
 
 
 class IssueFilter(django_filters.FilterSet):
-    cover_year = django_filters.NumberFilter(field_name="cover_date", lookup_expr="year")
-    cover_month = django_filters.NumberFilter(field_name="cover_date", lookup_expr="month")
+    cover_year = django_filters.NumberFilter(
+        label="Cover Year", field_name="cover_date", lookup_expr="year"
+    )
+    cover_month = django_filters.NumberFilter(
+        label="Cover Month", field_name="cover_date", lookup_expr="month"
+    )
     publisher_name = django_filters.filters.CharFilter(
-        field_name="series__publisher__name", lookup_expr="icontains"
+        label="Publisher Name", field_name="series__publisher__name", lookup_expr="icontains"
     )
     publisher_id = django_filters.filters.NumberFilter(
-        field_name="series__publisher__id", lookup_expr="exact"
+        label="Publisher Metron ID", field_name="series__publisher__id", lookup_expr="exact"
     )
-    series_name = django_filters.CharFilter(field_name="series__name", lookup_expr="icontains")
+    series_name = django_filters.CharFilter(
+        label="Series Name", field_name="series__name", lookup_expr="icontains"
+    )
     series_id = django_filters.filters.NumberFilter(
-        field_name="series__id", lookup_expr="exact"
+        label="Series Metron ID", field_name="series__id", lookup_expr="exact"
     )
     series_volume = django_filters.NumberFilter(
-        field_name="series__volume", lookup_expr="exact"
+        label="Series Volume Number", field_name="series__volume", lookup_expr="exact"
     )
     store_date_range = django_filters.filters.DateFromToRangeFilter("store_date")
     series_year_began = django_filters.filters.NumberFilter(
-        field_name="series__year_began", lookup_expr="exact"
+        label="Series Beginning Year", field_name="series__year_began", lookup_expr="exact"
+    )
+    sku = django_filters.filters.CharFilter(
+        label="Distributor SKU", field_name="sku", lookup_expr="iexact"
+    )
+    upc = django_filters.filters.CharFilter(
+        label="UPC Code", field_name="upc", lookup_expr="iexact"
     )
 
     class Meta:
