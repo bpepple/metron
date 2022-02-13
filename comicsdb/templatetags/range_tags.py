5efe77a5112ec6ceba7395bdccd8900cca20a636
Brian Pepple <bdpepple@gmail.com>
2022-02-13 14:47:00 -0500

    Inline variable that is immediately returned

--- comicsdb/templatetags/range_tags.py
+++ comicsdb/templatetags/range_tags.py
@@ -6,5 +6,4 @@ register = template.Library()
 @register.filter
 def cover_year_range(start, end):
     # Add 1 to end range to include current year
-    x = sorted(range(start, int(end) + 1), reverse=True)
-    return x
+    return sorted(range(start, int(end) + 1), reverse=True)
