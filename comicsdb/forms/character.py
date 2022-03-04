8160e329d18a4cdb957af10951c149740f0be993
Brian Pepple <bdpepple@gmail.com>
2022-03-04 14:54:41 -0500

    Drop unnecessary media files from Character form

--- comicsdb/forms/character.py
+++ comicsdb/forms/character.py
@@ -6,10 +6,6 @@ from comicsdb.models import Character
 
 
 class CharacterForm(ModelForm):
-    class Media:
-        css = {"all": ("admin/css/widgets.css",)}
-        js = (("/jsi18n"),)
-
     class Meta:
         model = Character
         fields = (
