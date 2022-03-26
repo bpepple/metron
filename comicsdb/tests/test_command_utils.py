8ad0a87063dee7f8dd2103ec236191c99b04212a
Brian Pepple <bdpepple@gmail.com>
2022-03-26 16:49:32 -0400

    Add test for determine_cover_date()

--- comicsdb/tests/test_command_utils.py
+++ comicsdb/tests/test_command_utils.py
@@ -1,6 +1,21 @@
 import datetime
 
-from comicsdb.management.commands._utils import get_week_range_from_store_date
+from comicsdb.management.commands._utils import (
+    determine_cover_date,
+    get_week_range_from_store_date,
+)
+
+
+def test_determine_cover_date_marvel():
+    result = determine_cover_date(datetime.date(2022, 1, 15), "Marvel Comics")
+    expected_result = datetime.date(2022, 3, 1)
+    assert result == expected_result
+
+
+def test_determine_cover_date_other_publisher():
+    result = determine_cover_date(datetime.date(2022, 1, 15), "Image")
+    expected_result = datetime.date(2022, 1, 1)
+    assert result == expected_result
 
 
 def test_week_range_from_store_date():
