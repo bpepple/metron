598d3841027e12faf57ec910b5e58cf70fa494c2
Brian Pepple <bdpepple@gmail.com>
2022-06-21 23:56:58 -0400

    Add admin action for TPB

--- comicsdb/admin/issue.py
+++ comicsdb/admin/issue.py
@@ -1,8 +1,9 @@
 import datetime
 from typing import Any, Optional
 
-from django.contrib import admin
+from django.contrib import admin, messages
 from django.db.models.query import QuerySet
+from django.utils.translation import ngettext
 from sorl.thumbnail.admin.current import AdminImageMixin
 
 from comicsdb.admin.util import AttributionInline
@@ -26,36 +27,6 @@ class FutureStoreDateListFilter(admin.SimpleListFilter):
 
         if self.value() == "nextWeek":
             return queryset.filter(store_date__week=week + 1, store_date__year=year)
-
-
-def add_dc_credits(modeladmin, request, queryset):
-    jim = Creator.objects.get(slug="jim-lee")
-    marie = Creator.objects.get(slug="marie-javins")
-    eic = Role.objects.get(name__iexact="editor in chief")
-    pub = Role.objects.get(name__iexact="publisher")
-    chief = Role.objects.get(name__iexact="Chief Creative Officer")
-    for i in queryset:
-        jc, create = Credits.objects.get_or_create(issue=i, creator=jim)
-        if create:
-            jc.role.add(pub, chief)
-        mc, create = Credits.objects.get_or_create(issue=i, creator=marie)
-        if create:
-            mc.role.add(eic)
-
-
-add_dc_credits.short_description = "Add current DC executive credits"
-
-
-def add_marvel_credits(modeladmin, request, queryset):
-    cb = Creator.objects.get(slug="c-b-cebulski")
-    eic = Role.objects.get(name__iexact="editor in chief")
-    for i in queryset:
-        cred, create = Credits.objects.get_or_create(issue=i, creator=cb)
-        if create:
-            cred.role.add(eic)
-
-
-add_marvel_credits.short_description = "Add current Marvel EIC"
 
 
 class CreditsInline(admin.TabularInline):
@@ -84,7 +55,7 @@ class IssueAdmin(AdminImageMixin, admin.ModelAdmin):
     autocomplete_fields = ["series", "characters", "teams", "arcs", "reprints"]
     list_select_related = ("series",)
     date_hierarchy = "cover_date"
-    actions = [add_dc_credits, add_marvel_credits]
+    actions = ["add_dc_credits", "add_marvel_credits", "add_tpb_info"]
     actions_on_top = True
     # form view
     fieldsets = (
@@ -115,3 +86,94 @@ class IssueAdmin(AdminImageMixin, admin.ModelAdmin):
         ),
     )
     inlines = (CreditsInline, VariantInline, AttributionInline)
+
+    @admin.action(description="Add current DC executive credits")
+    def add_dc_credits(self, request, queryset) -> None:
+        jim = Creator.objects.get(slug="jim-lee")
+        marie = Creator.objects.get(slug="marie-javins")
+        eic = Role.objects.get(name__iexact="editor in chief")
+        pub = Role.objects.get(name__iexact="publisher")
+        chief = Role.objects.get(name__iexact="Chief Creative Officer")
+        count = 0
+        for i in queryset:
+            modified = False
+            jc, create = Credits.objects.get_or_create(issue=i, creator=jim)
+            if create:
+                jc.role.add(pub, chief)
+                modified = True
+            mc, create = Credits.objects.get_or_create(issue=i, creator=marie)
+            if create:
+                mc.role.add(eic)
+                modified = True
+            if modified:
+                count += 1
+
+        self.message_user(
+            request,
+            ngettext(
+                "%d issue was updated.",
+                "%d issues were updated.",
+                count,
+            )
+            % count,
+            messages.SUCCESS,
+        )
+
+    @admin.action(description="Add current Marvel EIC")
+    def add_marvel_credits(self, request, queryset) -> None:
+        cb = Creator.objects.get(slug="c-b-cebulski")
+        eic = Role.objects.get(name__iexact="editor in chief")
+        count = 0
+        for i in queryset:
+            cred, create = Credits.objects.get_or_create(issue=i, creator=cb)
+            if create:
+                cred.role.add(eic)
+                count += 1
+
+        self.message_user(
+            request,
+            ngettext(
+                "%d issue was updated.",
+                "%d issues were updated.",
+                count,
+            )
+            % count,
+            messages.SUCCESS,
+        )
+
+    @admin.action(description="Add TPB info from reprints")
+    def add_tpb_info(self, request, queryset) -> None:
+        count = 0
+        for i in queryset:
+            modified = False
+            # Only add info for TPB.
+            if i.series.series_type.id == 10:
+                for reprint in i.reprints.all():
+                    # If reprint is not a single story let's bail.
+                    if reprint.name and len(reprint.name) < 2:
+                        modified = True
+                        # Add stories
+                        for story in reprint.name:
+                            i.name.append(story)
+                        i.save()
+                        # Add characters
+                        for character in reprint.characters.all():
+                            if character not in i.characters.all():
+                                i.characters.add(character)
+                        # Add Teams
+                        for team in reprint.teams.all():
+                            if team not in i.teams.all():
+                                i.teams.add(team)
+            if modified:
+                count += 1
+
+        self.message_user(
+            request,
+            ngettext(
+                "%d Trade Paperback was updated.",
+                "%d Trade Paperbacks were updated.",
+                count,
+            )
+            % count,
+            messages.SUCCESS,
+        )
