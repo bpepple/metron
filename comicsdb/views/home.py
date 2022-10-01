8f78a6448d19bdec528b2cfe6c1216a6fed6d15a
Brian Pepple <bdpepple@gmail.com>
2022-10-01 09:34:57 -0400

    Remove db stats from home page

--- comicsdb/views/home.py
+++ comicsdb/views/home.py
@@ -1,6 +1,6 @@
 from django.views.generic.base import TemplateView
 
-from comicsdb.models import Arc, Character, Creator, Issue, Publisher, Series, Team
+from comicsdb.models import Issue
 
 
 class HomePageView(TemplateView):
@@ -8,13 +8,6 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super(HomePageView, self).get_context_data(**kwargs)
-        context["publisher"] = Publisher.objects.count()
-        context["series"] = Series.objects.count()
-        context["issue"] = Issue.objects.count()
-        context["character"] = Character.objects.count()
-        context["creator"] = Creator.objects.count()
-        context["team"] = Team.objects.count()
-        context["arc"] = Arc.objects.count()
         context["recently_edited"] = (
             Issue.objects.prefetch_related("series", "series__series_type")
             .order_by("-modified")
