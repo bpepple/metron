ab405f2d80988e175d8190f833b1d1119a410aef
Brian Pepple <bdpepple@gmail.com>
2022-10-05 11:15:21 -0400

    Add __str__ to Rating model

--- comicsdb/models/rating.py
+++ comicsdb/models/rating.py
@@ -5,3 +5,6 @@ class Rating(models.Model):
     name = models.CharField(max_length=255)
     short_description = models.CharField(max_length=255, blank=True)
     description = models.TextField(blank=True)
+
+    def __str__(self) -> str:
+        return self.name
