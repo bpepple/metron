3b397e26ee3216c1891986912d8483408dc84d66
Brian Pepple <bdpepple@gmail.com>
2022-10-14 15:36:12 -0400

    Change Role widget size to 6 (#146)

--- comicsdb/forms/credits.py
+++ comicsdb/forms/credits.py
@@ -1,5 +1,5 @@
 from dal import autocomplete
-from django.forms import ModelChoiceField, ModelForm, inlineformset_factory
+from django.forms import ModelChoiceField, ModelForm, SelectMultiple, inlineformset_factory
 
 from comicsdb.models import Creator, Credits, Issue
 
@@ -19,6 +19,7 @@ class CreditsForm(ModelForm):
     class Meta:
         model = Credits
         fields = "__all__"
+        widgets = {"role": SelectMultiple(attrs={"size": 6})}
 
 
 CreditsFormSet = inlineformset_factory(
