cc585f3e644f5c472892ad4d34199692eec66880
Brian Pepple <bdpepple@gmail.com>
2022-10-15 23:30:01 -0400

    Disable signups temporarily

--- users/urls.py
+++ users/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("signup/", views.signup, name="signup"),
+    # path("signup/", views.signup, name="signup"),
     path(
         "account_activation_sent/",
         views.account_activation_sent,
