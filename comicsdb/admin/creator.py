0a42a8b52a1e07a46022c1137a2189c29c578332
Brian Pepple <bdpepple@gmail.com>
2023-02-21 08:31:55 -0500

    Add admin action for bad creator images

--- comicsdb/admin/creator.py
+++ comicsdb/admin/creator.py
@@ -1,4 +1,5 @@
-from django.contrib import admin
+from django.contrib import admin, messages
+from django.utils.translation import ngettext
 from sorl.thumbnail.admin.current import AdminImageMixin
 
 from comicsdb.admin.util import AttributionInline
@@ -13,3 +14,23 @@ class CreatorAdmin(AdminImageMixin, admin.ModelAdmin):
     readonly_fields = ("modified",)
     field = ("name", "slug", "modified", "birth", "death", "alias", "desc", "image")
     inlines = [AttributionInline]
+
+    @admin.action(description="Remove bad creator images")
+    def remove_bad_image(self, request, queryset) -> None:
+        count = 0
+        for i in queryset:
+            if i.image == "0":
+                i.image = ""
+                i.save()
+                count += 1
+
+        self.message_user(
+            request,
+            ngettext(
+                "%d creator image was fixed.",
+                "%d creators images were fixed.",
+                count,
+            )
+            % count,
+            messages.SUCCESS,
+        )
