1faa92759ae7d68dc910d5153a06208286dcdc67
Brian Pepple <bdpepple@gmail.com>
2023-04-10 07:46:49 -0400

    Add management comment to fix any bad issue stories

---
+++ comicsdb/management/commands/fix_issue_story.py
@@ -0,0 +1,19 @@
+from typing import Any, Optional
+
+from django.core.management.base import BaseCommand
+
+from comicsdb.models.issue import Issue
+
+
+class Command(BaseCommand):
+    help = "Fix bad issue story data."
+
+    def handle(self, *args: Any, **options: Any) -> Optional[str]:
+        qs = Issue.objects.filter(name=None)
+        if qs.count() > 0:
+            for i in qs:
+                i.name = []
+            fix_count = Issue.objects.bulk_update(qs, ["name"])
+            print(f"Fixed {fix_count} issues.")
+        else:
+            print("Nothing to fix.")
