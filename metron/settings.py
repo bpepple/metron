ef3178192021ea9377beda307a6e1d7b0c67c5bd
Brian Pepple <bdpepple@gmail.com>
2023-04-20 16:10:52 -0400

    Revert "Django-4.2 (#160)"
    This reverts commit a3da76443ad830f9e3d929a4bddbd21486a005f4.

--- metron/settings.py
+++ metron/settings.py
@@ -312,14 +312,12 @@ if not DEBUG:
     }
     AWS_LOCATION = "static"
 
-    STORAGES = {
-        "default": {"BACKEND": "metron.storage_backends.MediaStorage"},
-        "staticfiles": {"BACKEND": "storages.backends.s3boto3.S3Boto3Storage"},
-    }
+    STATICFILES_STORAGE = "storages.backends.s3boto3.S3Boto3Storage"
 
     STATICFILES_DIRS = (BASE_DIR / "static",)
     STATIC_URL = f"{AWS_S3_CUSTOM_DOMAIN}/{AWS_LOCATION}/"
 
+    DEFAULT_FILE_STORAGE = "metron.storage_backends.MediaStorage"
 else:
     STATICFILES_DIRS = (BASE_DIR / "static",)
     STATIC_URL = "/static/"
