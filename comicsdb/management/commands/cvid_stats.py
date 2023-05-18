5ca78e20c4d017fd1218dede27f242e155276ec4
Brian Pepple <bdpepple@gmail.com>
2023-05-18 11:35:15 -0400

    Add management command to get CVID stats

---
+++ comicsdb/management/commands/cvid_stats.py
@@ -0,0 +1,15 @@
+from typing import Any
+
+from django.core.management.base import BaseCommand
+
+from comicsdb.models.issue import Issue
+
+
+class Command(BaseCommand):
+    help = "Print Comic Vine ID statistics."
+
+    def handle(self, *args: Any, **options: Any) -> str | None:
+        issues = Issue.objects.all()
+        total = issues.count()
+        missing = issues.filter(cv_id__isnull=True).count()
+        print(f"Missing CV ID: {missing}\nWith CV ID: {total-missing}\nTotal Issues: {total}")
