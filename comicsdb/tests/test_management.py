fa10b6e31308449d628656bbefbb8453cde33b6c
Brian Pepple <bdpepple@gmail.com>
2023-05-31 13:46:55 -0400

    Add merge_arcs management command

--- comicsdb/tests/test_management.py
+++ comicsdb/tests/test_management.py
@@ -1,5 +1,6 @@
 from django.core.management import call_command
 
+from comicsdb.models.arc import Arc
 from comicsdb.models.character import Character
 
 
@@ -7,11 +8,19 @@ def test_merge_characters(superman: Character):
     cv_id = 9999
     desc = "Duplicate entry"
     alias = ["Clark Kent"]
-    test_character = Character.objects.create(
-        name="Superman", desc=desc, cv_id=cv_id, alias=alias
-    )
-    call_command("merge_characters", canonical=superman.id, other=test_character.id)
+    other_obj = Character.objects.create(name="Superman", desc=desc, cv_id=cv_id, alias=alias)
+    call_command("merge_characters", canonical=superman.id, other=other_obj.id)
     superman.refresh_from_db()
     assert superman.cv_id == cv_id
     assert superman.desc == desc
     assert superman.alias == alias
+
+
+def test_merge_arcs(fc_arc: Arc):
+    cv_id = 1000
+    desc = "Dup Arc"
+    other_obj = Arc.objects.create(name="Final Crisis", desc=desc, cv_id=cv_id)
+    call_command("merge_arcs", canonical=fc_arc.id, other=other_obj.id)
+    fc_arc.refresh_from_db()
+    assert fc_arc.cv_id == cv_id
+    assert fc_arc.desc == desc
