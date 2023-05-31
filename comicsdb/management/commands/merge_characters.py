4cc6131edfe3359538250abe1f2cdbd3a5693bf3
Brian Pepple <bdpepple@gmail.com>
2023-05-31 12:09:22 -0400

    Fix docstring

--- comicsdb/management/commands/merge_characters.py
+++ comicsdb/management/commands/merge_characters.py
@@ -1,5 +1,5 @@
 """
-PROCEED WITH CAUTION: uses deduplication fields to permanently merge author data objects
+PROCEED WITH CAUTION: uses deduplication fields to permanently merge Character data objects
 """
 from comicsdb.management.merge_command import MergeCommand
 from comicsdb.models.character import Character
