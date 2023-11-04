7b044eeb62d92eade847ee44ac501d1d3d4c1863
Brian Pepple <bdpepple@gmail.com>
2023-11-04 16:10:46 -0400

    Update `clean_issue_description` command to handle text that begins with 'Contains'

--- comicsdb/management/commands/clean_issue_description.py
+++ comicsdb/management/commands/clean_issue_description.py
@@ -11,8 +11,16 @@ class Command(BaseCommand):
 
     @staticmethod
     def _clean_desc(txt: str) -> str:
+        # No text, let's bail.
+        if not txt:
+            return ""
         split_txt = txt.split("\n\n")
         split_len = len(split_txt)
+        # If the description starts with 'Content' let's return an empty string.
+        if split_len < 2 and split_txt[0].lower().startswith("content"):
+            return ""
+        # If there are 2 or more paragraphs, check to see if the last paragraph starts with 'Content'
+        # and if so let's not join it to the return string.
         if split_len > 1:
             if split_txt[-1].lower().startswith("content"):
                 return "\n\n".join(split_txt[:-1]) if split_len > 2 else split_txt[0]
@@ -26,8 +34,8 @@ class Command(BaseCommand):
 
     def handle(self, *args: Any, **options: Any) -> None:
         series = Series.objects.get(id=options["series"])
-        print(series)
+        print(f"Updating: {series}")
         issues = Issue.objects.filter(series=series)
         for i in issues:
             i.desc = self._clean_desc(i.desc)
-        Issue.objects.bulk_update(issues, ["desc"], batch_size=250)
+        Issue.objects.bulk_update(issues, ["desc"], batch_size=100)
