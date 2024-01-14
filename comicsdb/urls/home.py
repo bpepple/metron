0d61901c598d9182192bfc147727364b22480cff
Brian Pepple <bdpepple@gmail.com>
2024-01-14 15:46:54 -0500

    Let's not cache the user session info

--- comicsdb/urls/home.py
+++ comicsdb/urls/home.py
@@ -1,11 +1,10 @@
 from django.urls import path
-from django.views.decorators.cache import cache_page
 
 from comicsdb.views.home import HomePageView
 from comicsdb.views.statistics import statistics
 
 app_name = ""
 urlpatterns = [
-    path("", cache_page(60 * 30)(HomePageView.as_view()), name="home"),
+    path("", HomePageView.as_view(), name="home"),
     path("statistics/", statistics, name="statistics"),
 ]
