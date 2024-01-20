0feeabe9768322fd26399ae55bbd7a58ccfbdbdf
Brian Pepple <bdpepple@gmail.com>
2024-01-20 15:00:55 -0500

    Add `universes` field to Team form, and detail template

--- comicsdb/forms/team.py
+++ comicsdb/forms/team.py
@@ -2,6 +2,7 @@ from django.forms import ClearableFileInput, ModelForm
 from django_select2 import forms as s2forms
 
 from comicsdb.forms.creator import CreatorsWidget
+from comicsdb.forms.universe import UniversesWidget
 from comicsdb.models import Team
 
 
@@ -12,8 +13,9 @@ class TeamsWidget(s2forms.ModelSelect2MultipleWidget):
 class TeamForm(ModelForm):
     class Meta:
         model = Team
-        fields = ("name", "desc", "creators", "cv_id", "image")
+        fields = ("name", "desc", "creators", "universes", "cv_id", "image")
         widgets = {
             "creators": CreatorsWidget(attrs={"class": "input"}),
+            "universes": UniversesWidget(attrs={"class": "input"}),
             "image": ClearableFileInput(),
         }
