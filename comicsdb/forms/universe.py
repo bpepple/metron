aab0805d1c462658333b0fdfbe87233f006bb5dc
Brian Pepple <bdpepple@gmail.com>
2024-01-20 14:23:13 -0500

    Add `UniverseForm`

---
+++ comicsdb/forms/universe.py
@@ -0,0 +1,15 @@
+from django.forms import ClearableFileInput, ModelForm
+from django_select2 import forms as s2forms
+
+from comicsdb.models import Universe
+
+
+class UniversesWidget(s2forms.ModelSelect2MultipleWidget):
+    search_fields = ["name__icontains"]
+
+
+class UniverseForm(ModelForm):
+    class Meta:
+        model = Universe
+        fields = ("publisher", "name", "designation", "desc", "image")
+        widgets = {"image": ClearableFileInput()}
