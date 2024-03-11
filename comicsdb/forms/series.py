cea68f7fa41210a605a9872d93dae01cb9072f50
Brian Pepple <bdpepple@gmail.com>
2024-03-11 08:50:26 -0400

    Add `Series` form validation for cv_id field
    Comic Vine is consolidating their Trade Paperback series, and re-using the old `id` for new series, which could cause issues if we capture this information right now.

--- comicsdb/forms/series.py
+++ comicsdb/forms/series.py
@@ -3,6 +3,10 @@ from django_select2 import forms as s2forms
 
 from comicsdb.models import Series
 
+# Series_Type objects id's
+TPB = 10
+HC = 8
+
 
 class SeriesWidget(s2forms.ModelSelect2Widget):
     search_fields = ["name__icontains"]
@@ -66,13 +70,27 @@ class SeriesForm(ModelForm):
             "associated",
         ]
 
-    def clean_associated(self):
+    def clean_cv_id(self) -> any:
+        cvid = self.cleaned_data["cv_id"]
+        if cvid:
+            series_type = self.cleaned_data["series_type"]
+            # Don't cv_id for Trade Paperbacks. Refer to:
+            # https://github.com/bpepple/metron/issues/219
+            if series_type.id == TPB:
+                msg = (
+                    "Adding a Comic Vine ID  is not allowed for Trade Paperbacks, "
+                    "due to the consolidation work being done there."
+                )
+                raise ValidationError(msg)
+        return cvid
+
+    def clean_associated(self) -> any:
         assoc = self.cleaned_data["associated"]
         if assoc:
             series_type = self.cleaned_data["series_type"]
             # If adding an associated series and self.series_type is a TPB or HC
             # raise a validation error.
-            if series_type.id in [8, 10]:
+            if series_type.id in [HC, TPB]:
                 raise ValidationError(
                     "Collections are not allowed to have an associated series."
                 )
