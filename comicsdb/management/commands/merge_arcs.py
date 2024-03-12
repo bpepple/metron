cb773cdeda566b3730d4ecff82bd6a0c6f4de1df
Brian Pepple <bdpepple@gmail.com>
2024-03-12 09:25:32 -0400

    Formatting change

--- comicsdb/management/commands/merge_arcs.py
+++ comicsdb/management/commands/merge_arcs.py
@@ -1,6 +1,7 @@
 """
 PROCEED WITH CAUTION: uses deduplication fields to permanently merge Arc data objects
 """
+
 from comicsdb.management.merge_command import MergeCommand
 from comicsdb.models.arc import Arc
 
