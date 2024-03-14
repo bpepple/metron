28325494850023dccc65ad19492c019ef2675d03
Brian Pepple <bdpepple@gmail.com>
2024-03-14 15:29:56 -0400

    Remove original variant if replacing with new

--- comicsdb/models/variant.py
+++ comicsdb/models/variant.py
@@ -1,7 +1,13 @@
+import contextlib
+import logging
+
+from django.core.exceptions import ObjectDoesNotExist
 from django.db import models
 from sorl.thumbnail import ImageField
 
 from comicsdb.models.issue import Issue
+
+LOGGER = logging.getLogger(__name__)
 
 
 class Variant(models.Model):
@@ -17,3 +23,15 @@ class Variant(models.Model):
 
     def __str__(self) -> str:
         return self.name
+
+    def save(self, *args, **kwargs) -> None:
+        # Let's delete the original image if we're replacing it by uploading a new one.
+        with contextlib.suppress(ObjectDoesNotExist):
+            this = Variant.objects.get(id=self.id)
+            if this.image and this.image != self.image:
+                if self.image:
+                    LOGGER.info("Replacing '%s' with '%s'", this.image, self.image)
+                else:
+                    LOGGER.info("Replacing '%s' with 'None'.", this.image)
+                this.image.delete(save=False)
+        return super().save(*args, **kwargs)
