c8787e00be5e59846981328e033c7b360ac87d92
Brian Pepple <bdpepple@gmail.com>
2024-04-13 13:47:26 -0400

    Remove depreciated DEBUG_TOOLBAR setting (#234)

--- metron/settings.py
+++ metron/settings.py
@@ -341,7 +341,6 @@ else:
         "debug_toolbar.panels.templates.TemplatesPanel",
         "debug_toolbar.panels.cache.CachePanel",
         "debug_toolbar.panels.signals.SignalsPanel",
-        "debug_toolbar.panels.logging.LoggingPanel",
         "debug_toolbar.panels.redirects.RedirectsPanel",
     ]
 
