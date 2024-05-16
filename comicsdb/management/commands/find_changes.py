59d720b5b17f14a735a7bd8294162aa94ebcff6a
Brian Pepple <bdpepple@gmail.com>
2024-05-16 09:49:58 -0400

    Refactor the find_changes command

--- comicsdb/management/commands/find_changes.py
+++ comicsdb/management/commands/find_changes.py
@@ -16,37 +16,17 @@ class Command(BaseCommand):
         search_date = datetime.strptime(options["date"], "%Y-%m-%d").date()
         admin = CustomUser.objects.get(id=1)
 
-        arcs = Arc.objects.filter(modified__date=search_date).exclude(edited_by=admin)
-        characters = Character.objects.filter(modified__date=search_date).exclude(edited_by=admin)
-        creators = Creator.objects.filter(modified__date=search_date).exclude(edited_by=admin)
-        issues = Issue.objects.filter(modified__date=search_date).exclude(edited_by=admin)
-        publishers = Publisher.objects.filter(modified__date=search_date).exclude(edited_by=admin)
-        teams = Team.objects.filter(modified__date=search_date).exclude(edited_by=admin)
+        models = [Arc, Issue, Character, Creator, Team, Publisher]
+        results: list[dict] = []
+        for mod in models:
+            qs = mod.objects.filter(modified__date=search_date).exclude(edited_by=admin)
+            results.append({"model": mod, "queryset": qs})
 
-        title = f"Changes for {search_date}\n------------------------"
-
+        title = f"Changes for {search_date}\n-----------------------"
         self.stdout.write(self.style.SUCCESS(title))
-        if arcs:
-            self.stdout.write(self.style.SUCCESS("\nStory Arcs:"))
-            for arc in arcs:
-                self.stdout.write(self.style.WARNING(f"\t'{arc}' changed by '{arc.edited_by}'"))
-        if characters:
-            self.stdout.write(self.style.SUCCESS("\nCharacters:"))
-            for character in characters:
-                self.stdout.write(self.style.WARNING(f"\t'{character}' changed by '{character.edited_by}'"))
-        if creators:
-            self.stdout.write(self.style.SUCCESS("\nCreators:"))
-            for creator in creators:
-                self.stdout.write(self.style.WARNING(f"\t'{creator}' changed by '{creator.edited_by}'"))
-        if issues:
-            self.stdout.write(self.style.SUCCESS("\nIssues:"))
-            for issue in issues:
-                self.stdout.write(self.style.WARNING(f"\t'{issue}' changed by '{issue.edited_by}'"))
-        if publishers:
-            self.stdout.write(self.style.SUCCESS("\nPublishers:"))
-            for publisher in publishers:
-                self.stdout.write(self.style.WARNING(f"\t'{publisher}' changed by '{publisher.edited_by}'"))
-        if teams:
-            self.stdout.write(self.style.SUCCESS("\nTeams:"))
-            for team in teams:
-                self.stdout.write(self.style.WARNING(f"\t'{team}' changed by '{team.edited_by}'"))
+
+        for item in results:
+            if item["queryset"]:
+                self.stdout.write(self.style.SUCCESS(f"\n{item['model'].__name__}:"))
+                for obj in item["queryset"]:
+                    self.stdout.write(self.style.WARNING(f"\t'{obj}' changed by '{obj.edited_by}'"))
