191364c5129e5156da7a1bc1e5ed0fe5a8be5108
Brian Pepple <bdpepple@gmail.com>
2024-05-16 10:00:34 -0400

    Refactor the annual_stats command

--- comicsdb/management/commands/annual_stats.py
+++ comicsdb/management/commands/annual_stats.py
@@ -12,26 +12,16 @@ class Command(BaseCommand):
         return super().add_arguments(parser)
 
     def handle(self, *args: any, **options: any) -> None:
+        results: list[dict] = []
         users = CustomUser.objects.filter(date_joined__year=options["year"]).count()
-        comics = Issue.objects.filter(created_on__year=options["year"]).count()
-        characters = Character.objects.filter(created_on__year=options["year"]).count()
-        creators = Creator.objects.filter(created_on__year=options["year"]).count()
-        teams = Team.objects.filter(created_on__year=options["year"]).count()
-        arcs = Arc.objects.filter(created_on__year=options["year"]).count()
-        publishers = Publisher.objects.filter(created_on__year=options["year"]).count()
-        series = Series.objects.filter(created_on__year=options["year"]).count()
+        results.append({"model": CustomUser, "count": users})
+
+        models = [Arc, Character, Creator, Issue, Publisher, Series, Team]
+        for mod in models:
+            count = mod.objects.filter(created_on__year=options["year"]).count()
+            results.append({"model": mod, "count": count})
 
         title = f"{options['year']} New Additions Statistics"
         self.stdout.write(self.style.SUCCESS(f"{title}\n{len(title) * '-'}"))
-        self.stdout.write(
-            self.style.SUCCESS(
-                f"Users: {users:,}\n"
-                f"Publishers: {publishers:,}\n"
-                f"Series: {series:,}\n"
-                f"Comics: {comics:,}\n"
-                f"Characters: {characters:,}\n"
-                f"Creators: {creators:,}\n"
-                f"Teams: {teams:,}\n"
-                f"Arcs: {arcs:,}\n"
-            )
-        )
+        for result in results:
+            self.stdout.write(self.style.WARNING(f"{result['model'].__name__}: {result['count']:,}"))
